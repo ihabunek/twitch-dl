70c92c1e1bd3c381155b3d454fd5183bd520bdc1
BluelyTASer <59511669+BluelyTASer@users.noreply.github.com>
2020-10-24 07:07:40 -0300

    Update commands.py

--- twitchdl/commands.py
+++ twitchdl/commands.py
@@ -136,7 +136,7 @@ def _join_vods(playlist_path, target, overwrite, anion):
   print("Size in bytes is: " + str(size))
   global command
   if size > 1999999999:
-    command = 'ffmpeg -hwaccel cuvid -hwaccel_output_format cuda -vcodec h264 -i "' + playlist_path + '" -b:a 96000 -b:v ' + str((1999999999 * 5.5) / video_length) + ' -preset ultrafast -c:a copy -s 1280x720 -r 30 "' + str(target) +  '" -stats -loglevel warning'
+    command = 'ffmpeg -hwaccel cuvid -hwaccel_output_format cuda -vcodec h264 -i "' + playlist_path + '" -b:a 96000 -b:v ' + str((999999999 * 6) / video_length) + ' -preset ultrafast -c:a copy -s 1280x720 -r 30 "' + str(target) +  '" -stats -loglevel warning'
   else: 
     command = 'ffmpeg -i "' + str(playlist_path) + '" -c copy "' + str(target) + '" -stats -loglevel warning'
 
