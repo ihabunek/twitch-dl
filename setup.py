b43c9dc9b9f99f472d61256faa146266e45a7b1e
Ivan Habunek <ivan@habunek.com>
2022-08-18 09:36:56 +0200

    Use double quotes please

--- setup.py
+++ setup.py
@@ -10,33 +10,33 @@ makes it faster.
 """
 
 setup(
-    name='twitch-dl',
-    version='1.22.0',
-    description='Twitch downloader',
+    name="twitch-dl",
+    version="1.22.0",
+    description="Twitch downloader",
     long_description=long_description.strip(),
-    author='Ivan Habunek',
-    author_email='ivan@habunek.com',
-    url='https://github.com/ihabunek/twitch-dl/',
+    author="Ivan Habunek",
+    author_email="ivan@habunek.com",
+    url="https://github.com/ihabunek/twitch-dl/",
     project_urls={
         "Documentation": "https://twitch-dl.bezdomni.net/"
     },
-    keywords='twitch vod video download',
-    license='GPLv3',
+    keywords="twitch vod video download",
+    license="GPLv3",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
     ],
     packages=find_packages(),
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     install_requires=[
         "m3u8>=1.0.0,<2.0.0",
         "httpx>=0.17.0,<1.0.0",
     ],
     entry_points={
-        'console_scripts': [
-            'twitch-dl=twitchdl.console:main',
+        "console_scripts": [
+            "twitch-dl=twitchdl.console:main",
         ],
     }
 )
