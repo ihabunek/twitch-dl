5a43a4388c51c345c59c046a2b04a3b1e8ef0eec
Ivan Habunek <ivan@habunek.com>
2023-04-18 07:08:46 +0200

    Bump version, add changelog

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ makes it faster.
 
 setup(
     name="twitch-dl",
-    version="2.1.1",
+    version="2.1.2",
     description="Twitch downloader",
     long_description=long_description.strip(),
     author="Ivan Habunek",
--- twitchdl/__init__.py
+++ twitchdl/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 CLIENT_ID = "kimne78kx3ncx6brgo4mv6wki5h1ko"
