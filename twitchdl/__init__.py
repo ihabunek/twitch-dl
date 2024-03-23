40d52891f8cfe86a7cc972a781658636d6452a4e
Ivan Habunek <ivan@habunek.com>
2024-03-23 08:55:56 +0100

    Don't hardcode version number

--- twitchdl/__init__.py
+++ twitchdl/__init__.py
@@ -1,3 +1,8 @@
-__version__ = "2.1.4"
+from importlib import metadata
+
+try:
+    __version__ = metadata.version("twitch-dl")
+except metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
 
 CLIENT_ID = "kd1unb4b3q4t58fwlpcbzcbnm76a8fp"
