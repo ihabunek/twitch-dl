cf1693b500e1010ed3afb0047db13b7ef979dd7b
Ivan Habunek <ivan@habunek.com>
2024-03-26 10:23:35 +0100

    Fix env command

--- twitchdl/commands/env.py
+++
@@ -1,9 +0,0 @@
-import platform
-import sys
-import twitchdl
-
-
-def env(args):
-    print("twitch-dl", twitchdl.__version__)
-    print("Platform:", platform.platform())
-    print("Python", sys.version)
