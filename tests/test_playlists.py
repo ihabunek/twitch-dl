e1cbc952f793e2e9a4fb6fd138188cb282903330
Ivan Habunek <ivan@habunek.com>
2025-04-06 11:22:48 +0200

    Add test for 4k video

--- tests/test_playlists.py
+++ tests/test_playlists.py
@@ -2,19 +2,18 @@ import pytest
 
 from twitchdl import twitch
 from twitchdl.entities import VideosType
-from twitchdl.playlists import parse_playlists
+from twitchdl.playlists import Playlist, parse_playlists
 from twitchdl.playlists_auth import fetch_auth_playlist
 
-TEST_CHANNEL = "baertaffy"
 
-
+@pytest.mark.parametrize("channel", ["baertaffy"])
 @pytest.mark.parametrize("type", ["archive", "highlight", "upload"])
-def test_sub_only_playlists(type: VideosType):
+def test_sub_only_playlists(channel: str, type: VideosType):
     """
     This tests two methods of accessing playlists, one used in normal operation,
     and the other when the video is sub-only and compares the results.
     """
-    video = _get_video_by_type(type)
+    video = _get_video(channel, type)
     access_token = twitch.get_access_token(video["id"])
     playlists_text = twitch.get_playlists(video["id"], access_token)
     regular_playlists = sorted(parse_playlists(playlists_text), key=lambda p: p.group_id)
@@ -23,9 +22,21 @@ def test_sub_only_playlists(type: VideosType):
     assert regular_playlists == subonly_playlists
 
 
-def _get_video_by_type(type: VideosType):
+def test_sub_only_playlists_4k():
+    # Known 4k video
+    video = twitch.get_video("2177476725")
+    assert video is not None
+    access_token = twitch.get_access_token(video["id"])
+    playlists_text = twitch.get_playlists(video["id"], access_token)
+    regular_playlists = sorted(parse_playlists(playlists_text), key=lambda p: p.group_id)
+    subonly_playlists = sorted(fetch_auth_playlist(video), key=lambda p: p.group_id)
+
+    assert regular_playlists == subonly_playlists
+
+
+def _get_video(channel: str, type: VideosType):
     """Fetch the latest video of given type from a randomly chosen channel"""
-    videos = twitch.get_channel_videos(TEST_CHANNEL, limit=1, sort="time", type = type)
+    videos = twitch.get_channel_videos(channel, limit=1, sort="time", type = type)
     if not videos["edges"]:
         raise ValueError(f"Video of type '{type}' not found.")
     return videos["edges"][0]["node"]
